cda87733f64e22e5804d9112bb201547f3880537
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-12 22:53:31 +0100

    Started the Meme component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,12 +4,12 @@ import Trollface from "../images/troll-face.png"
 
 export default function Header() {
     return (
-        <nav>
-            <div className="nav--logo">
+        <header>
+            <div className="header--logo">
                 <img src={Trollface} />
                 <h1>Meme generator</h1>
             </div>
             <p>React Course - Project 3</p>
-        </nav>
+        </header>
     )
 }
---
+++ src/components/Meme.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <section className="meme">
+            <input type="text" />
+            <input type="text" />
+            <button type="submit">Get a new meme image</button>
+        </section>
+    )
+}
