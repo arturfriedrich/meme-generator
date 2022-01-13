5642f04fc62394809681f6afecddb2caa6761c4f
Artúr Friedrich <artur.frierdrich.harka@gmail.com>
2022-01-13 21:09:13 +0100

    just messing with git in terminal mode

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -6,6 +6,17 @@ export default function Meme() {
 
     const [memeImage, setMemeImage] = useState()
 
+    /**
+     * Challenge: Save the random meme URL in state
+     * - Create new state called `memeImage` with an
+     *   empty string as default
+     * - When the getMemeImage function is called, update
+     *   the `memeImage` state to be the random chosen
+     *   image URL
+     * - Below the div.form, add an <img /> and set the
+     *   src to the new `memeImage` state you created
+     */
+
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
