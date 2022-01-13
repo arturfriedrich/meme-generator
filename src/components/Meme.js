28d5dcf301a7c22296ccb18e409c3991d7185455
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-01-13 08:15:25 +0100

    Added random image fetch

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import memesData from "../memesData"
+
 export default function Meme() {
+
+    let url
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        url = memesArray[randomNumber].url
+    }
+
     return (
         <main>
-            <form className="form">
+            <div className="form">
                 <input
                     type="text"
                     placeholder="Top text"
@@ -16,10 +27,11 @@ export default function Meme() {
                 />
                 <button
                     className="form--button"
+                    onClick={getMemeImage}
                 >
                     Get a new meme image
                 </button>
-            </form>
+            </div>
         </main>
     )
 }
