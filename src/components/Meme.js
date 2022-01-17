2f8f360d19fcd7db4a3624c8d7bffeaceb536f56
Artúr Friedrich <artur.frierdrich.harka@gmail.com>
2022-01-17 19:48:16 +0100

    Update Meme.js

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -4,14 +4,6 @@ import memesData from "../memesData"
 
 export default function Meme() {
 
-    /**
-     * Challenge: 
-     * 1. Set up the text inputs to save to
-     *    the `topText` and `bottomText` state variables.
-     * 2. Replace the hard-coded text on the image with
-     *    the text being saved to state.
-     */
-
     const [meme, setMeme] = useState({
         topText: "",
         bottomText: "",
